677d1da055fbf1a71ef0c80044f6b3b203c6ad32
yuewy <15757129442@163.com>
2019-02-15 17:21:48 +0800

    add some code

---
+++ Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnXml.java
@@ -0,0 +1,21 @@
+package com.course.testng.multiThread;
+
+import org.testng.annotations.Test;
+
+public class MultiThreadOnXml {
+
+    @Test
+    public void test1(){
+        System.out.println("Thread Id:"+Thread.currentThread().getId());
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Thread Id:"+Thread.currentThread().getId());
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Thread Id:"+Thread.currentThread().getId());
+    }
+}
