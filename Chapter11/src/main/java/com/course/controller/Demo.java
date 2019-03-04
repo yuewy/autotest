48466aa44ac12b97f1468248b255b6bae56675f2
wuyue <15757129442@163.com>
2019-03-04 18:06:12 +0800

    add database operation

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,13 +1,12 @@
 package com.course.controller;
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,4 +24,19 @@ public class Demo {
         return template.selectOne("getUserCount");
     }
 
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public  int addUser(@RequestBody User user){
+        return template.insert("addUser",user);
+    }
+
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET)
+    public int delUser(@RequestParam int id){
+        return template.delete("deleteUser",id);
+    }
 }
