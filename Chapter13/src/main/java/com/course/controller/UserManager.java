2f1a367d278d31c7aafcc5e895f684430f0fa63a
wuyue <15757129442@163.com>
2019-03-12 18:13:24 +0800

    update code

--- Chapter13/src/main/java/com/course/controller/UserManager.java
+++ Chapter13/src/main/java/com/course/controller/UserManager.java
@@ -1,10 +1,8 @@
 package com.course.controller;
 
 import com.course.model.User;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jws.Oneway;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Objects;
 
-@Log4j
 @RestController
 @Api(value = "v1",description = "用户管理系统")
 @RequestMapping("v1")
@@ -43,7 +38,7 @@ public class UserManager {
 
 
     @ApiOperation(value = "添加用户接口",httpMethod = "POST")
-    @RequestMapping(value = "addUser",method = RequestMethod.POST)
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
     public boolean addUser(HttpServletRequest request,@RequestBody User user){
         Boolean x = verifyCookies(request);
         int result =0;
@@ -57,7 +52,7 @@ public class UserManager {
     }
 
     @ApiOperation(value = "获取用户（列表）信息接口",httpMethod = "POST")
-    @RequestMapping(value = "getUserInfo",method = RequestMethod.POST)
+    @RequestMapping(value = "/getUserInfo",method = RequestMethod.POST)
     public List<User> getUserInfo(HttpServletRequest request,@RequestBody User user){
         Boolean x = verifyCookies(request);
         if (x == true){
